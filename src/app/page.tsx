d9169754b355f06edc0eb71b782c4facc8f31dfa
Joel Jarro <rodrigoquiroz308@gmail.com>
2023-06-24 15:07:19 -0400

    fix image button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,19 @@
 import CustomButton from "@/components/interactive/CustomButton";
 import ImageButton from "@/components/interactive/ImageButton";
+import { UserCircle } from "iconoir-react";
 
 export default function Home() {
-    return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-white">
-            <div className="flex flex-col items-center justify-center gap-4">
-                <CustomButton>Login</CustomButton>
-                <CustomButton theme="secondary">More news</CustomButton>
-                <CustomButton theme="imageButton">
-                    <ImageButton />
-                </CustomButton>
-            </div>
-        </main>
-    );
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-white">
+      <div className="flex flex-col items-center justify-center gap-4">
+        <CustomButton>Login</CustomButton>
+        <CustomButton theme="secondary">More news</CustomButton>
+        <CustomButton theme="imageButton">
+          <ImageButton>
+            <UserCircle />
+          </ImageButton>
+        </CustomButton>
+      </div>
+    </main>
+  );
 }
--- src/components/interactive/ImageButton.tsx
+++ src/components/interactive/ImageButton.tsx
@@ -1,15 +1,19 @@
 "use client";
 import React from "react";
-import { IconoirProvider, ProfileCircle } from "iconoir-react";
 
-function ImageButton() {
+interface ImageButtonProps {
+  children: React.ReactNode;
+}
+
+function ImageButton({ children }: ImageButtonProps) {
   return (
     <div className="flex gap-2 items-center">
-      <ProfileCircle
-        height={"1.75em"}
-        width={"1.75em"}
-        className="bg-secondary rounded-full text-black group-hover:bg-primary"
-      />
+      <div
+        style={{ width: "1.7em", height: "1.7em" }}
+        className="bg-secondary rounded-full text-black group-hover:bg-primary content-center flex items-center justify-center"
+      >
+        {children}
+      </div>
       <p>Join now!</p>
     </div>
   );
