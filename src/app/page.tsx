bcd684b69aa8ab07f07c6bf2a5ceb424e7cfeaa0
Joel Jarro <rodrigoquiroz308@gmail.com>
2023-06-24 19:41:10 -0400

    Revert "fix problems"
    This reverts commit 9af95e4411c926e770bf4f70f32cf1bd5f6175ea.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,7 @@
 
 import CustomButton from "@/components/interactive/CustomButton";
 import ImageButton from "@/components/interactive/ImageButton";
-import { JavascriptOriginal } from "devicons-react";
-import { javascript } from "@codemirror/lang-javascript";
-import { dracula } from "@uiw/codemirror-theme-dracula";
-import dynamic from "next/dynamic";
-import { lineNumbersRelative } from "@/utils/@codemirror/glutter/src";
-
-const CodeMirror = dynamic(() => import("@uiw/react-codemirror"), {
-  ssr: false,
-});
+import { JavascriptOriginal } from 'devicons-react';
 
 export default function Home() {
   return (
@@ -21,15 +13,7 @@ export default function Home() {
         <CustomButton theme="imageButton">
           <ImageButton icon={<JavascriptOriginal />}>Join now!</ImageButton>
         </CustomButton>
-        <CodeMirror
-          value="console.log('hello world!');"
-          height="100svh"
-          width="40em"
-          theme={dracula}
-          extensions={[javascript({ jsx: true }), lineNumbersRelative]}
-          onChange={(value) => console.log(value)}
-          placeholder={"// Enter your code here"}
-        />
+
       </div>
     </main>
   );
