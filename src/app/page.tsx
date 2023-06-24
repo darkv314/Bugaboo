7ae9032041a01dc77ac260e4523546c6ed08b2d1
Joel Jarro <rodrigoquiroz308@gmail.com>
2023-06-24 18:40:57 -0400

    base for Code Mirror

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,14 @@
+"use client";
+
 import CustomButton from "@/components/interactive/CustomButton";
 import ImageButton from "@/components/interactive/ImageButton";
-import { JavascriptOriginal } from 'devicons-react';
+import { JavascriptOriginal } from "devicons-react";
+import { javascript } from "@codemirror/lang-javascript";
+import { dracula } from "@uiw/codemirror-theme-dracula";
+import dynamic from "next/dynamic";
+const CodeMirror = dynamic(() => import("@uiw/react-codemirror"), {
+  ssr: false,
+});
 
 export default function Home() {
   return (
@@ -9,10 +17,16 @@ export default function Home() {
         <CustomButton>Login</CustomButton>
         <CustomButton theme="secondary">More news</CustomButton>
         <CustomButton theme="imageButton">
-          <ImageButton icon={<JavascriptOriginal />}>
-            Join now!
-          </ImageButton>
+          <ImageButton icon={<JavascriptOriginal />}>Join now!</ImageButton>
         </CustomButton>
+        <CodeMirror
+          value="console.log('hello world!');"
+          height="10em"
+          width="30em"
+          theme={dracula}
+          extensions={[javascript({ jsx: true })]}
+          onChange={(value) => console.log(value)}
+        />
       </div>
     </main>
   );
