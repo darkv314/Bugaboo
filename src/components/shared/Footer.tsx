cab291e4810f3dbc48a8d0b74e13b2cf16e586f9
Joel Jarro <rodrigoquiroz308@gmail.com>
2023-06-25 17:41:25 -0400

    responsive footer

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -7,16 +7,16 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="bg-dark py-10 sm:px-72 w-full backdrop-blur-md">
-      <div className="flex items-center justify-center md:justify-between gap-3">
+    <footer className="bg-dark py-10 lg:px-72 w-full backdrop-blur-md">
+      <div className="flex items-center justify-center lg:justify-between gap-3">
         <div className="flex items-start self-start h-full gap-44">
           <div className="h-full flex flex-col items-start gap-8">
             <Title />
             <SocialIcons />
           </div>
-          <div className="w-px h-64 bg-secondaryDark"></div>
+          <div className="hidden lg:block w-px h-64 bg-secondaryDark"></div>
         </div>
-        <div className="h-full items-start gap-24 justify-end flex">
+        <div className="hidden h-full items-start gap-24 justify-end lg:flex">
           <Links linksList={navBarNoAuth} isRow={false} />
           <div className="flex flex-col items-center justify-center gap-4 self-center">
             <CustomButton theme="secondary">Join Us</CustomButton>
