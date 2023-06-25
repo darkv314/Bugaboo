9baf4e9ef4bbe54627fd0d1ac31e421bc3fbcd91
Joel Jarro <rodrigoquiroz308@gmail.com>
2023-06-25 15:30:51 -0400

    add link bugaboo

--- src/components/shared/TopBar.tsx
+++ src/components/shared/TopBar.tsx
@@ -6,15 +6,18 @@ import {
   LinkedIn,
   ChatLines,
 } from "iconoir-react";
+import Link from "next/link";
 
 export default function Topbar() {
   return (
     <nav className="bg-dark w-full py-4 sm:px-72 backdrop-blur-md flex items-center justify-center md:justify-between">
-      <div className="flex-shrink-0 flex gap-2">
-        <ChatLines className="text-xl text-primary" />
-        <span style={cabin.style} className="text-xl font-bold text-white">
-          Bugaboo
-        </span>
+      <div>
+        <Link className="flex-shrink-0 flex gap-2" href="/">
+          <ChatLines className="text-xl text-primary" />
+          <span style={cabin.style} className="text-xl text-primary font-bold">
+            Bugaboo
+          </span>
+        </Link>
       </div>
       <div className="hidden md:block">
         <ul className="flex gap-5 items-center">
