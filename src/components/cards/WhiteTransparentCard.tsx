7691965011491edf984a04251805b38f1b026123
Joel Jarro <rodrigoquiroz308@gmail.com>
2023-06-25 20:23:17 -0400

    add hero image

--- src/components/cards/WhiteTransparentCard.tsx
+++ src/components/cards/WhiteTransparentCard.tsx
@@ -1,15 +1,19 @@
 import { ReactNode } from "react";
 
 type WhiteTransparentCardProps = {
-    children: ReactNode;
+  children: ReactNode;
+  className?: string;
 };
 
-function WhiteTransparentCard({ children }: WhiteTransparentCardProps) {
-    return (
-        <div className="flex flex-col rounded-2xl items-center bg-white/85 w-full sm:w-1/2 md:w-1/5 m-4 py-12 px-10 gap-2 backdrop-blur-md">
-            {children}
-        </div>
-    );
+function WhiteTransparentCard({
+  children,
+  className,
+}: WhiteTransparentCardProps) {
+  return (
+    <div className={`rounded-2xl bg-white/80 backdrop-blur-md ${className}`}>
+      {children}
+    </div>
+  );
 }
 
 export default WhiteTransparentCard;
--- src/components/shared/NavBar.tsx
+++ src/components/shared/NavBar.tsx
@@ -5,17 +5,19 @@ import { Links } from "./Links";
 import { navBarNoAuth } from "@/data/data";
 
 export default function Navbar() {
-    return (
-        <nav className="bg-dark w-full py-10 sm:px-72 backdrop-blur-md flex items-center justify-center xl:justify-between">
-            <div className="hidden xl:block">
-                <Links linksList={navBarNoAuth} isRow />
-            </div>
-            <div className="flex sm:flex-row flex-col gap-5 items-center">
-                <CustomButton theme={"secondary"}>Login</CustomButton>
-                <CustomButton theme="imageButton">
-                    <ImageButton icon={<SquareCursor />}>Join now!</ImageButton>
-                </CustomButton>
-            </div>
-        </nav>
-    );
+  return (
+    <nav className="bg-dark w-full py-10 sm:px-72 backdrop-blur-md flex items-center justify-center xl:justify-between">
+      <div className="hidden xl:block">
+        <Links linksList={navBarNoAuth} isRow />
+      </div>
+      <div className="flex sm:flex-row flex-col gap-5 items-center">
+        <CustomButton theme={"secondary"}>Login</CustomButton>
+        <CustomButton theme="imageButton">
+          <ImageButton icon={<SquareCursor />}>
+            <span className="whitespace-nowrap">Join now!</span>
+          </ImageButton>
+        </CustomButton>
+      </div>
+    </nav>
+  );
 }
