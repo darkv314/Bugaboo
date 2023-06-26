a28c565b85e56993351d4cac5bfcc3c76d669ec1
Joel Jarro <rodrigoquiroz308@gmail.com>
2023-06-25 20:59:18 -0400

    fix badge width

--- src/components/badge/Badge.tsx
+++ src/components/badge/Badge.tsx
@@ -4,12 +4,12 @@ const badgeConfig = {
   primary: {
     bgColor: "bg-primary",
     textColor: "text-dark",
-    width: "w-full min-w-[7em]",
+    width: "w-min",
   },
   secondary: {
     bgColor: "bg-secondary",
     textColor: "text-dark",
-    width: "w-full min-w-[7em]",
+    width: "w-min",
   },
 };
 
@@ -20,12 +20,14 @@ type ButtonProps = {
 
 function Badge({ theme = "primary", children }: ButtonProps) {
   return (
-    <span
-      className={`${badgeConfig[theme]?.bgColor} 
-      ${badgeConfig[theme]?.textColor} text-base py-2 px-3 rounded-3xl uppercase ${badgeConfig[theme]?.width}`}
-    >
-      {children}
-    </span>
+    <div>
+      <span
+        className={`${badgeConfig[theme]?.bgColor} 
+      ${badgeConfig[theme]?.textColor}  text-base py-2 px-3 rounded-3xl uppercase`}
+      >
+        {children}
+      </span>
+    </div>
   );
 }
 
