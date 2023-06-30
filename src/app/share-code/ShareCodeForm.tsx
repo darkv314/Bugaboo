ac78614d65c62467c74c04af8427663bed596fbf
Vanessa Tejerina <vanetejerina314@gmail.com>
2023-06-29 20:52:44 -0400

    Fixed the padding in the share code form

--- src/app/share-code/ShareCodeForm.tsx
+++ src/app/share-code/ShareCodeForm.tsx
@@ -13,101 +13,101 @@ import { useState } from "react";
 import { javascript } from "@codemirror/lang-javascript";
 
 type ShareCodeFormInputs = {
-  title: string;
-  description: string;
-  language: string;
-  code: string;
+    title: string;
+    description: string;
+    language: string;
+    code: string;
 };
 
 function ShareCodeForm() {
-  const methods = useForm<ShareCodeFormInputs>();
-  const router = useRouter();
-  const { auth, setAuth } = useAuth();
-  const [code, setCode] = useState<string>("");
+    const methods = useForm<ShareCodeFormInputs>();
+    const router = useRouter();
+    const { auth, setAuth } = useAuth();
+    const [code, setCode] = useState<string>("");
 
-  const shareCodeMutation = useMutation({
-    mutationFn: (data: ShareCodeFormInputs) => {
-      return shareCode(auth.token, data);
-    },
-    onSuccess: (response: AxiosResponse) => {
-      console.log(response);
-      setAuth({
-        userId: response.data.user.id,
-        token: response.data.jwt,
-        username: response.data.user.username,
-      });
-      //   router.push("/shared-codes");
-    },
-    onError: (error: any) => {
-      console.log(error);
-    },
-  });
+    const shareCodeMutation = useMutation({
+        mutationFn: (data: ShareCodeFormInputs) => {
+            return shareCode(auth.token, data);
+        },
+        onSuccess: (response: AxiosResponse) => {
+            console.log(response);
+            setAuth({
+                userId: response.data.user.id,
+                token: response.data.jwt,
+                username: response.data.user.username,
+            });
+            //   router.push("/shared-codes");
+        },
+        onError: (error: any) => {
+            console.log(error);
+        },
+    });
 
-  function onSubmit(data: ShareCodeFormInputs) {
-    data.code = code;
-    console.log(data);
+    function onSubmit(data: ShareCodeFormInputs) {
+        data.code = code;
+        console.log(data);
 
-    //uncomment when code is ready
-    //shareCodeMutation.mutate(data);
-  }
+        //uncomment when code is ready
+        //shareCodeMutation.mutate(data);
+    }
 
-  return (
-    <>
-      <div className="relative flex flex-col rounded-2xl items-center bg-white/80 gap-2">
-        <h1 className="text-3xl text-black font-extrabold font-cabin">
-          Share Code!🤓
-        </h1>
-        <FormProvider {...methods}>
-          <form
-            className="w-full flex flex-col gap-4"
-            onSubmit={methods.handleSubmit(onSubmit)}
-          >
-            <span>
-              <CustomInput
-                id="title"
-                label="Title"
-                type="text"
-                validations={{
-                  required: "Title is required",
-                }}
-              />
-              <CustomInput
-                id="description"
-                label="Description"
-                type="textarea"
-                validations={{
-                  required: "Description is required",
-                }}
-              />
-              <CustomInput
-                id="language"
-                label="Language"
-                type="multiselect"
-                validations={{
-                  required: "Language is required",
-                }}
-              />
-            </span>
-            <CodeMirror
-              height="400px"
-              theme={"dark"}
-              onChange={(value) => {
-                setCode(value);
-              }}
-              extensions={[javascript()]}
-              placeholder={"// Enter your code here"}
-            />
-            <CustomButton theme="secondary">
-              <LoadingLabel
-                message="Share Code"
-                state={shareCodeMutation.isLoading}
-              />
-            </CustomButton>
-          </form>
-        </FormProvider>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className="relative flex flex-col rounded-2xl items-center bg-white/80 gap-2">
+                <h1 className="text-3xl text-black font-extrabold font-cabin">
+                    Share Code!🤓
+                </h1>
+                <FormProvider {...methods}>
+                    <form
+                        className="w-full flex flex-col gap-4"
+                        onSubmit={methods.handleSubmit(onSubmit)}
+                    >
+                        <span>
+                            <CustomInput
+                                id="title"
+                                label="Title"
+                                type="text"
+                                validations={{
+                                    required: "Title is required",
+                                }}
+                            />
+                            <CustomInput
+                                id="description"
+                                label="Description"
+                                type="textarea"
+                                validations={{
+                                    required: "Description is required",
+                                }}
+                            />
+                            <CustomInput
+                                id="language"
+                                label="Language"
+                                type="multiselect"
+                                validations={{
+                                    required: "Language is required",
+                                }}
+                            />
+                        </span>
+                        <CodeMirror
+                            height="400px"
+                            theme={"dark"}
+                            onChange={(value) => {
+                                setCode(value);
+                            }}
+                            extensions={[javascript()]}
+                            placeholder={"// Enter your code here"}
+                        />
+                        <CustomButton theme="secondary">
+                            <LoadingLabel
+                                message="Share Code"
+                                state={shareCodeMutation.isLoading}
+                            />
+                        </CustomButton>
+                    </form>
+                </FormProvider>
+            </div>
+        </>
+    );
 }
 
 export default ShareCodeForm;
--- src/app/share-code/page.tsx
+++ src/app/share-code/page.tsx
@@ -1,11 +1,11 @@
 import ShareCodeForm from "./ShareCodeForm";
 
 function page() {
-  return (
-    <main className="min-h-screen bg-white px-4 lg:px-72 py-40 flex flex-col gap-16">
-      <ShareCodeForm />
-    </main>
-  );
+    return (
+        <main className="min-h-screen bg-white px-4 lg:px-72 py-8 flex flex-col">
+            <ShareCodeForm />
+        </main>
+    );
 }
 
 export default page;
