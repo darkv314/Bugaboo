0c5e55982e32185c39937410308a5eaf6837c070
Joel Jarro <rodrigoquiroz308@gmail.com>
2023-06-30 11:17:18 -0400

    show the line commented on  modal

--- src/app/code/[id]/CommentCodeForm.tsx
+++ src/app/code/[id]/CommentCodeForm.tsx
@@ -12,87 +12,90 @@ import { Dispatch } from "react";
 import { FormProvider, useForm } from "react-hook-form";
 
 type CommentCodeFormInputs = {
-    message: string;
+  message: string;
 };
 
 type CommentCodeFormProps = {
-    setModalOpen: Dispatch<React.SetStateAction<boolean>>;
-    codeLine: string;
-    numberCodeLine: number;
+  setModalOpen: Dispatch<React.SetStateAction<boolean>>;
+  codeLine: string;
+  numberCodeLine: number;
 };
 
 function CommentCodeForm({
-    setModalOpen,
-    codeLine,
-    numberCodeLine,
+  setModalOpen,
+  codeLine,
+  numberCodeLine,
 }: CommentCodeFormProps) {
-    const methods = useForm<CommentCodeFormInputs>();
-    const { auth } = useAuth();
-    const { id } = useParams();
+  const methods = useForm<CommentCodeFormInputs>();
+  const { auth } = useAuth();
+  const { id } = useParams();
 
-    const commentMutation = useMutation({
-        mutationFn: (data: CommentCodeFormInputs) => {
-            return commentService.postComment(auth.token, {
-                message: data.message,
-                code: Number(id),
-                upvotes: 0,
-                downvotes: 0,
-                users_permissions_user: Number(auth.userId),
-                codeLine,
-                numberCodeLine,
-            });
-        },
-        onSuccess: (response: AxiosResponse) => {
-            console.log(response);
-            setModalOpen(false);
-        },
-        onError: (error: any) => {
-            console.log(error);
-        },
-    });
+  const commentMutation = useMutation({
+    mutationFn: (data: CommentCodeFormInputs) => {
+      return commentService.postComment(auth.token, {
+        message: data.message,
+        code: Number(id),
+        upvotes: 0,
+        downvotes: 0,
+        users_permissions_user: Number(auth.userId),
+        codeLine,
+        numberCodeLine,
+      });
+    },
+    onSuccess: (response: AxiosResponse) => {
+      console.log(response);
+      setModalOpen(false);
+    },
+    onError: (error: any) => {
+      console.log(error);
+    },
+  });
 
-    function onSubmit(data: CommentCodeFormInputs) {
-        commentMutation.mutate(data);
-    }
+  function onSubmit(data: CommentCodeFormInputs) {
+    commentMutation.mutate(data);
+  }
 
-    return (
-        <>
-            <div className="relative flex flex-col rounded-2xl items-center bg-white/80 gap-2">
-                <Badge theme="secondary" id="comment">
-                    COMMENT
-                </Badge>
-                <FormProvider {...methods}>
-                    <form
-                        className="w-full flex flex-col gap-4"
-                        onSubmit={methods.handleSubmit(onSubmit)}
-                    >
-                        <span>
-                            <CustomInput
-                                id="message"
-                                rows={4}
-                                label="Comment"
-                                type="textarea"
-                                validations={{
-                                    required: "Comment is required",
-                                }}
-                            />
-                        </span>
-                        <div className="flex gap-2">
-                            <CustomButton theme="secondary">
-                                <LoadingLabel
-                                    message="Comment"
-                                    state={commentMutation.isLoading}
-                                />
-                            </CustomButton>
-                            <CustomButton onClick={() => setModalOpen(false)}>
-                                Cancel
-                            </CustomButton>
-                        </div>
-                    </form>
-                </FormProvider>
+  return (
+    <>
+      <div className="relative flex flex-col rounded-2xl items-center bg-white/80 gap-2">
+        <Badge theme="secondary" id="comment">
+          COMMENT
+        </Badge>
+        <p className="text-sm text-gray-400">
+          Commenting on line <span className="font-bold text-gray-800">{codeLine}</span>
+        </p>
+        <FormProvider {...methods}>
+          <form
+            className="w-full flex flex-col gap-4"
+            onSubmit={methods.handleSubmit(onSubmit)}
+          >
+            <span>
+              <CustomInput
+                id="message"
+                rows={4}
+                label="Comment"
+                type="textarea"
+                validations={{
+                  required: "Comment is required",
+                }}
+              />
+            </span>
+            <div className="flex gap-2">
+              <CustomButton theme="secondary">
+                <LoadingLabel
+                  message="Comment"
+                  state={commentMutation.isLoading}
+                />
+              </CustomButton>
+              <CustomButton onClick={() => setModalOpen(false)}>
+                Cancel
+              </CustomButton>
             </div>
-        </>
-    );
+          </form>
+        </FormProvider>
+      </div>
+    </>
+  );
 }
 
 export default CommentCodeForm;
--- src/app/code/[id]/page.tsx
+++ src/app/code/[id]/page.tsx
@@ -19,103 +19,99 @@ import ImageButton from "@/components/interactive/ImageButton";
 import { SquareCursor, User } from "iconoir-react";
 
 type CommentLine = {
-    from: number;
-    number: number;
-    text: string;
-    to: number;
+  from: number;
+  number: number;
+  text: string;
+  to: number;
 };
 
 export default function Page() {
-    const params = useParams();
-    const { id } = params;
-    const router = useRouter();
-    const { auth, setAuth } = useAuth();
-    const [code, setCode] = useState<Code>();
-    const [modalOpen, setModalOpen] = useState(false);
-    const [commentLine, setCommentLine] = useState<CommentLine>({
-        from: 0,
-        number: 0,
-        text: "",
-        to: 0,
+  const params = useParams();
+  const { id } = params;
+  const router = useRouter();
+  const { auth, setAuth } = useAuth();
+  const [code, setCode] = useState<Code>();
+  const [modalOpen, setModalOpen] = useState(false);
+  const [commentLine, setCommentLine] = useState<CommentLine>({
+    from: 0,
+    number: 0,
+    text: "",
+    to: 0,
+  });
+
+  const handleComment = (line: LineModel) => {
+    console.log(line);
+    setCommentLine(line);
+    setModalOpen(true);
+  };
+
+  useEffect(() => {
+    codeService.getCode(auth.token, Number(id)).then((res) => {
+      setCode(res.data.attributes);
     });
+  }, []);
 
-    const handleComment = (line: LineModel) => {
-        console.log(line);
-        setCommentLine(line);
-        setModalOpen(true);
-    };
-
-    useEffect(() => {
-        codeService.getCode(auth.token, Number(id)).then((res) => {
-            setCode(res.data.attributes);
-        });
-    }, []);
-
-    return (
-        <main className="min-h-screen bg-white px-4 lg:px-72 py-8 flex flex-col gap-4">
-            <Modal
-                setOpen={setModalOpen}
-                isOpen={modalOpen}
-                onClose={() => {
-                    setModalOpen(false);
+  return (
+    <main className="min-h-screen bg-white px-4 lg:px-72 py-8 flex flex-col gap-4">
+      <Modal
+        setOpen={setModalOpen}
+        isOpen={modalOpen}
+        onClose={() => {
+          setModalOpen(false);
+        }}
+      >
+        {auth.token ? (
+          <CommentCodeForm
+            setModalOpen={setModalOpen}
+            codeLine={commentLine?.text}
+            numberCodeLine={commentLine?.number}
+          />
+        ) : (
+          <div className="flex flex-col gap-4 items-center">
+            <h1 className="text-2xl font-bold font-cabin">
+              You need to be logged in to comment 😢
+            </h1>
+            <div className="flex gap-2  w-full">
+              <CustomButton
+                onClick={() => {
+                  router.push(`/login?callbackUrl=/code/${id}`);
                 }}
-            >
-                {auth.token ? (
-                    <CommentCodeForm
-                        setModalOpen={setModalOpen}
-                        codeLine={commentLine?.text}
-                        numberCodeLine={commentLine?.number}
-                    />
-                ) : (
-                    <div className="flex flex-col gap-4 items-center">
-                        <h1 className="text-2xl font-bold font-cabin">
-                            You need to be logged in to comment 😢
-                        </h1>
-                        <div className="flex gap-2  w-full">
-                            <CustomButton
-                                onClick={() => {
-                                    router.push(
-                                        `/login?callbackUrl=/code/${id}`
-                                    );
-                                }}
-                                theme="secondary"
-                            >
-                                Login
-                            </CustomButton>
+                theme="secondary"
+              >
+                Login
+              </CustomButton>
 
-                            <CustomButton
-                                onClick={() => {
-                                    router.push("/sign-up");
-                                }}
-                                theme="imageButton"
-                            >
-                                <ImageButton icon={<SquareCursor />}>
-                                    Sign Up
-                                </ImageButton>
-                            </CustomButton>
-                        </div>
-                    </div>
-                )}
-            </Modal>
-            <Badge theme="secondary" id="codes">
-                CODES
-            </Badge>
-            <h1 className="text-4xl font-bold font-cabin">{code?.title}</h1>
-            <p className="text-lg">{code?.description}</p>
-            {/* *TODO  language */}
-            <CodeMirror
-                height="400px"
-                theme={"dark"}
-                extensions={[
-                    javascript(),
-                    linesAddPlusGutter(handleComment, {
-                        backgroundColor: "#3E404B",
-                    }),
-                ]}
-                editable={false}
-                value={code?.code}
-                placeholder={"// Enter your code here"}
-            />
-        </main>
-    );
+              <CustomButton
+                onClick={() => {
+                  router.push("/sign-up");
+                }}
+                theme="imageButton"
+              >
+                <ImageButton icon={<SquareCursor />}>Sign Up</ImageButton>
+              </CustomButton>
+            </div>
+          </div>
+        )}
+      </Modal>
+      <Badge theme="secondary" id="codes">
+        CODES
+      </Badge>
+      <h1 className="text-4xl font-bold font-cabin">{code?.title}</h1>
+      <p className="text-lg">{code?.description}</p>
+      {/* *TODO  language */}
+      <CodeMirror
+        height="400px"
+        theme={"dark"}
+        extensions={[
+          javascript(),
+          linesAddPlusGutter(handleComment, {
+            backgroundColor: "#3E404B",
+          }),
+        ]}
+        editable={false}
+        value={code?.code}
+        placeholder={"// Enter your code here"}
+      />
+    </main>
+  );
 }
