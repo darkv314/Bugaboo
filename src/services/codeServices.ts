d7e73ac6c2a92cd93750d397041c6f55c21c0c47
Vanessa Tejerina <vanetejerina314@gmail.com>
2023-06-30 09:51:12 -0400

    Changed the route for code services

--- src/services/codeServices.ts
+++ src/services/codeServices.ts
@@ -2,11 +2,9 @@ import axios from "@/api/axios";
 import { Code } from "@/models/code";
 import { AxiosResponse } from "axios";
 
-
-
 export const codeService = {
     async getCodes(token: string) {
-        const url = `/codes`;
+        const url = `/codes?populate=*`;
         const response = await axios.get(url, {
             headers: {
                 "Content-Type": "application/json",
@@ -16,38 +14,37 @@ export const codeService = {
         });
         return response.data;
     },
-    async postCode(
-        token: string,
-        code: Code
-    ) {
+    async postCode(token: string, code: Code) {
         const url = `/codes`;
-        const response = await axios.post(url, { data: code }, {
-            headers: {
-                "Content-Type": "application/json",
-                Authorization: `Bearer ${token}`,
-            },
-            withCredentials: true,
-        });
+        const response = await axios.post(
+            url,
+            { data: code },
+            {
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${token}`,
+                },
+                withCredentials: true,
+            }
+        );
         return response.data;
     },
-    async putCode(
-        token: string,
-        code: Code
-    ) {
+    async putCode(token: string, code: Code) {
         const url = `/codes/${code.id}`;
-        const response = await axios.put(url, { data: code }, {
-            headers: {
-                "Content-Type": "application/json",
-                Authorization: `Bearer ${token}`,
-            },
-            withCredentials: true,
-        });
+        const response = await axios.put(
+            url,
+            { data: code },
+            {
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${token}`,
+                },
+                withCredentials: true,
+            }
+        );
         return response.data;
     },
-    async deleteCode(
-        token: string,
-        code: Code
-    ) {
+    async deleteCode(token: string, code: Code) {
         const url = `/codes/${code.id}`;
         const response = await axios.delete(url, {
             headers: {
@@ -58,10 +55,7 @@ export const codeService = {
         });
         return response.data;
     },
-    async getCode(
-        token: string,
-        id: number
-    ) {
+    async getCode(token: string, id: number) {
         const url = `/codes/${id}`;
         const response = await axios.get(url, {
             headers: {
@@ -71,6 +65,5 @@ export const codeService = {
             withCredentials: true,
         });
         return response.data;
-    }
-
-};     
+    },
+};
