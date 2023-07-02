012abcc3ac80de58724a95b539d4e74e1046cb97
Vanessa Tejerina <vanetejerina314@gmail.com>
2023-07-02 11:38:15 -0400

    Added a nice message for comments in notifications

--- src/services/commentServices.ts
+++ src/services/commentServices.ts
@@ -1,7 +1,8 @@
-import { StrapiResponse } from './../models/strapiModel';
-import { CommentGetI, PostCommentI, PutCommentI } from './../models/comment';
+import { StrapiResponse } from "./../models/strapiModel";
+import { CommentGetI, PostCommentI, PutCommentI } from "./../models/comment";
 import axios from "@/api/axios";
 import { CommentI } from "@/models/comment";
+import { notificationService } from "./notificationService";
 
 export const commentService = {
     async getComments(token: string) {
@@ -20,13 +21,21 @@ export const commentService = {
         comment: PostCommentI
     ): Promise<StrapiResponse<CommentGetI>> {
         const url = `/comments`;
-        const response = await axios.post(url, { data: comment }, {
-            headers: {
-                "Content-Type": "application/json",
-                Authorization: `Bearer ${token}`,
-            },
-            withCredentials: true,
+        await notificationService.createNotification(token, {
+            message: `Tienes un nuevo comentario en el código ${comment.codeTitle}} de ${comment.usernameComment}`,
+            codeUserId: comment.codeUserId,
         });
+        const response = await axios.post(
+            url,
+            { data: comment },
+            {
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${token}`,
+                },
+                withCredentials: true,
+            }
+        );
         return response.data;
     },
     async putComment(
@@ -35,20 +44,20 @@ export const commentService = {
         id: number
     ): Promise<StrapiResponse<CommentGetI>> {
         const url = `/comments/${id}`;
-        const response = await axios.put(url, { data: comment }, {
-            headers: {
-                "Content-Type": "application/json",
-                Authorization: `Bearer ${token}`,
-            },
-            withCredentials: true,
-        });
+        const response = await axios.put(
+            url,
+            { data: comment },
+            {
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${token}`,
+                },
+                withCredentials: true,
+            }
+        );
         return response.data;
     },
-    async deleteComment(
-        token: string,
-        comment: CommentI,
-        id: number
-    ) {
+    async deleteComment(token: string, comment: CommentI, id: number) {
         const url = `/comments/${id}`;
         const response = await axios.delete(url, {
             headers: {
@@ -69,7 +78,5 @@ export const commentService = {
             withCredentials: true,
         });
         return response;
-    }
-
-
-};     
+    },
+};
